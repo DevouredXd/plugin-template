36ee35e5737bd129cab5805dc1cbfb5d3babf6f4
DevouredXd <mccjhon9@gmail.com>
2025-04-01 20:27:51 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { before } from "@vendetta/patcher";
+import { findByName } from "@vendetta/metro";
+
+const MessageActions = findByName("MessageActions");
 
 export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+  onLoad() {
+    const unpatch = before("default", MessageActions, (args) => {
+      const buttons = args[0]?.children;
+      if (!Array.isArray(buttons)) return;
+
+      const deleteIndex = buttons.findIndex(b => b?.props?.action === "delete");
+      if (deleteIndex > 0) {
+        const [deleteBtn] = buttons.splice(deleteIndex, 1);
+        buttons.unshift(deleteBtn);
+      }
+    });
+
+    return () => unpatch();
+  }
+};
